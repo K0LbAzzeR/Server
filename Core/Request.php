2d76b6fae8130317d6bf238964d5d2f4793d2960
Mofsy <ru.mofsy@yandex.ru>
2015-06-03 00:10:03 +0300

    Init release

---
+++ Core/Request.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Armature
+ *
+ * @link          https://github.com/Armature
+ * @author        Oleg Budrin <ru.mofsy@yandex.ru>
+ * @author_link  https://mofsy.ru
+ * @copyright     Copyright (c) 2015, Oleg Budrin (Mofsy)
+ */
+
+namespace Armature\Core;
+
+
+class Request {
+
+	/*
+	 * Аякс запрос
+	 */
+	public $ajax = false;
+
+	/*
+	 * Метод запроса
+	 */
+	public $method;
+
+
+	public function __construct()
+	{
+		echo 'refrf';
+	}
+
+}
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Armature
+ *
+ * @link          https://github.com/Armature
+ * @author        Oleg Budrin <ru.mofsy@yandex.ru>
+ * @author_link  https://mofsy.ru
+ * @copyright     Copyright (c) 2015, Oleg Budrin (Mofsy)
+ */
+
+define('ROOT_DIR', __DIR__);
+define('CORE_DIR', __DIR__ . '/Core');
+
+/*
+ * Автозагрузчик
+ */
+require CORE_DIR . '/Loader.php';
+$loader = new \Armature\Core\Loader;
+$loader->register();
+$loader->addNamespace('Armature\Core', CORE_DIR);
+
+$request = new Armature\Core\Request();
+$app = new Armature\Core\App($request);
+
+/*
+ * Запускаем сервер
+ */
+$app->run();

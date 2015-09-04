cab0f2c19607236094ffd51727a50262313b92ce
Mofsy <ru.mofsy@yandex.ru>
2015-09-05 00:57:53 +0300

    Minor fix

--- index.php
+++ index.php
@@ -10,11 +10,6 @@
   +----------------------------------------------------------------------+
 */
 
-@error_reporting(E_ALL ^ E_WARNING ^ E_NOTICE);
-@ini_set('display_errors', true);
-@ini_set('html_errors', true);
-@ini_set('error_reporting', E_ALL ^ E_WARNING ^ E_NOTICE);
-
 define('VERSION', '0.1.0');
 define('ROOT_DIR', __DIR__);
 define('CORE_DIR', __DIR__ . '/Core');
@@ -42,24 +37,3 @@ $handler = new Armature\Core\Handler($request);
 $response->make();
 
 echo $timer->get();
-
-// "ловим" строку запроса, превращаем её в масив
-$routeArray = explode('/', $_SERVER['REQUEST_URI']);
-// удаляем пустые элементы массива (элементы образованные начальным и конечным слэшами URI)
-// тут можно было обойтить array_shift и array_pop - но мне способ с foreach кажется более "универсальным"
-$route = array();
-foreach ($routeArray as $value) {
-    if (!empty($value)) {
-        $route[] = trim($value);
-    }
-
-}
-// вводим в адресную строку всякий бред, смотрим что нам показывают
-echo '<pre>';
-print_r($route);
-echo '</pre>';
-
-var_dump($request);
-var_dump($handler);
-var_dump($response);
-var_dump($config);

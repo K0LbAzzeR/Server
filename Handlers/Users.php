bbf31f8255529f5a350fd1a5ef459590dad3c4e5
Mofsy <ru.mofsy@yandex.ru>
2015-09-05 18:41:06 +0300

    Init Users handler

---
+++ Handlers/Users.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  +----------------------------------------------------------------------+
+  | Armature                                                             |
+  +----------------------------------------------------------------------+
+  | Website: http://armature.pw                                          |
+  | Github: https://github.com/Armature                                  |
+  | License: http://creativecommons.org/licenses/by-nc-sa/4.0/           |
+  +----------------------------------------------------------------------+
+  | Author: Oleg Budrin (Mofsy) <support@mofsy.ru> <https://mofsy.ru>    |
+  +----------------------------------------------------------------------+
+*/
+
+namespace Armature\Handlers;
+
+
+class Users {
+
+}
